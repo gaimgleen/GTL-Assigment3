481bffd3ea374d87cd9ef78ebb3d595c3dcea2ca
gaimgleen <68541583+gaimgleen@users.noreply.github.com>
2020-08-21 09:43:40 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Gaim from './week-3/Daniel.js';
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <Gaim/>
+      </div>
+  );
+}
+
+export default App;
